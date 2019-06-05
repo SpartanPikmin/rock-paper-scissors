253c47152258605d0abef54943de1755aebff85e
Devon Crane <cranede@10-116-10-104.wireless.oregonstate.edu>
2019-06-05 11:54:47 -0700

    final assesment submition for sp19, Thanks for the great class Yong

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,46 @@
 class Game
 
-  attr_reader :started
+  attr_reader :started, :finished
 
   def start
     @started = true
+    @finished = false
   end
 
   def started?
     self.started || false
   end
 
+  def finished?
+    if(self.finished)
+      self.finished
+    else
+      self.finished
+    end
+  end
+
+  #ideas for the challenge that I dont have time to test:
+  #1) extract play into its own object and have game.play(left, right) call play.play(left,right)
+  #     (nameing needs to change in play I know), and return the output of play.play
+  #2) extract/delegate the play method into smaller methods
+  #3) do what we did in the bottles exersise and see if there are any commonalities in each statment. 
+  #     atfirst I had a lot of elifs but then I condensed them into 4 if/elsif statments. could potentially 
+  #      condence further too.
+
   def play(left = nil, right = nil)
     raise 'Game must first be started' unless started?
     return nil unless (left && right)
-    if (left == :rock && right == :scissors)
+    if ((left == :rock && right == :scissors) || left == :scissors && right == :rock)
+      @finished = true
       "Rock beats scissors!"
+    elsif((left == :paper && right == :rock) || (left == :rock && right == :paper))
+      @finished = true
+      "Paper beats rock!"
+    elsif((left == :scissors && right == :paper) || (left == :paper && right == :scissors))
+      @finished = true
+      "Scissors beats paper!"
+    elsif((left == :scissors && right == :scissors) || (left == :paper && right == :paper) || (left == :rock && right == :rock))
+      "Tie game. Try again!"
     else
       :UNKNOWN
     end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,34 +23,36 @@ describe 'A game of rock paper scissors' do
 
   describe 'playing' do
 
+    let(:game){Game.new}
+    #game.start
+    #idea was to reduce repeatition in my test code.
+    
+    #i also feel that, there should only be one assertion per it block.
+    #was not sure how to split up the blocks and have the naming be meaning full
+
     context 'without starting' do
       it 'can not be played if it has not started' do
-        game = Game.new
         expect { game.play }.to raise_error(RuntimeError)
       end
     end
 
     context 'after starting' do
       it 'can be played if it has started' do
-        game = Game.new
         game.start
         expect { game.play }.to_not raise_error
       end
 
       it 'returns nil when not provided with anything' do
-        game = Game.new
         game.start
         expect(game.play).to be_nil
       end
 
       it 'returns nil when only provided one rock, paper or scissor' do
-        game = Game.new
         game.start
         expect(game.play(:FAKE)).to be_nil
       end
 
       it 'does not return nil when two objects are provided' do
-        game = Game.new
         game.start
         expect(game.play(:FAKE, :FAKE)).to_not be_nil
       end
@@ -58,28 +60,56 @@ describe 'A game of rock paper scissors' do
 
     context 'rock vs scissors' do
       it 'announces the correct winner' do
-        game = Game.new
         game.start
         expect(game.play(:rock, :scissors)).to eq("Rock beats scissors!")
         expect(game.play(:scissors, :rock)).to eq("Rock beats scissors!")
       end
-      skip('is finished')
+      it 'is finished' do
+        game.start
+        game.play(:rock, :scissors)
+        expect(game.finished?()).to eq(true)
+      end
     end
 
     context 'rock vs paper' do
-      skip('returns "Paper beats rock!"')
-      skip('is finished')
+      it 'returns "Paper beats rock!"' do
+        game.start
+        expect(game.play(:paper, :rock)).to eq("Paper beats rock!")
+        expect(game.play(:rock, :paper)).to eq("Paper beats rock!")
+      end
+      it 'is finished' do
+        game.start
+        game.play(:paper, :rock)
+        expect(game.finished?()).to eq(true)
+      end
     end
 
 
     context 'scissors vs paper' do
-      skip('returns "Scissors vs paper!"')
-      skip('is finished')
+      it 'returns "Scissors beats paper!"' do
+        game.start
+        expect(game.play(:scissors, :paper)).to eq("Scissors beats paper!")
+        expect(game.play(:paper, :scissors)).to eq("Scissors beats paper!")
+      end
+      it 'is finished' do
+        game.start
+        game.play(:scissors, :paper)
+        expect(game.finished?()).to eq(true)
+      end
     end
 
     context 'a tie game' do
-      skip('returns "Tie game. Try again!"')
-      skip('is not finished')
+      it 'returns "Tie game. Try again!"' do
+        game.start
+        expect(game.play(:scissors, :scissors)).to eq("Tie game. Try again!")
+        expect(game.play(:paper, :paper)).to eq("Tie game. Try again!")
+        expect(game.play(:rock, :rock)).to eq("Tie game. Try again!")
+      end 
+      it 'is not finished' do
+        game.start
+        expect(game.play(:scissors, :scissors)).to eq("Tie game. Try again!")
+        expect(game.finished?()).to eq(false)
+      end
     end
 
   end
